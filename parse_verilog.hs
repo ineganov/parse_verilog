ab690eec31db94c72460a0de44609baac4c07222
Ilya Neganov <ineganov@gmail.com>
2020-01-20 00:39:46 +0000

    better lexer, remove ad-hoc parser

--- parse_verilog.hs
+++ parse_verilog.hs
@@ -1,11 +1,9 @@
 import System.Environment
 import Data.Char
-import Data.Map (fromList, member, (!))
 import Control.Monad.State.Lazy (State, get, put, runState)
 
 data Token = Comment String 
            | Ident String
-           | DotConn String
            | NumLiteral Int 
            | OpAssign | OpBitwiseOr | OpBitwiseAnd | OpBitwiseXor | OpBitwiseInv
            | K_module | K_endmodule | K_begin | K_end | K_assign | K_wire | K_reg | K_input | K_output | K_inout
@@ -14,7 +12,8 @@ data Token = Comment String
            | LBrace   | RBrace
            | Semi 
            | Colon
-           | Hash 
+           | Hash
+           | Dot 
            | BaseHex | BaseDec | BaseOct | BaseBin
            | Comma deriving (Show, Eq)
 
@@ -25,16 +24,8 @@ data Port  = Port String Dir Width   deriving (Show)
 
 data Module = Module String [Port]   deriving (Show)
 
-keyw_map = fromList [ ("module",    K_module    ),
-                      ("endmodule", K_endmodule ),
-                      ("begin",     K_begin     ),
-                      ("end",       K_end       ),
-                      ("assign",    K_assign    ),
-                      ("wire",      K_wire      ),
-                      ("reg",       K_reg       ),
-                      ("input",     K_input     ),
-                      ("output",    K_output    ),
-                      ("inout",     K_inout     ) ]
+pp :: Show a => [a] -> IO ()
+pp = mapM_ (putStrLn . show)
 
 tokenize :: String -> [Token]
 tokenize []           = []
@@ -50,24 +41,32 @@ tokenize (';':xs)     = Semi     : tokenize xs
 tokenize (':':xs)     = Colon    : tokenize xs
 tokenize (',':xs)     = Comma    : tokenize xs
 tokenize ('#':xs)     = Hash     : tokenize xs
+tokenize ('.':xs)     = Dot      : tokenize xs
 tokenize ('=':xs)     = OpAssign     : tokenize xs
 tokenize ('&':xs)     = OpBitwiseAnd : tokenize xs
 tokenize ('|':xs)     = OpBitwiseOr  : tokenize xs
 tokenize ('^':xs)     = OpBitwiseXor : tokenize xs
 tokenize ('~':xs)     = OpBitwiseInv : tokenize xs
-tokenize ('\\':xs)    = Ident   (takeWhile (/= ' ') xs)     : tokenize (dropWhile (/= ' ') xs)
-tokenize ('.':xs)     = DotConn (takeWhile isLegalIdent xs) : tokenize (dropWhile isLegalIdent xs)
-tokenize ('/':'/':xs) = Comment (takeWhile (/= '\n') xs)    : tokenize (dropWhile (/= '\n') xs)
+tokenize ('\\':xs)    = Ident   idnt : tokenize rest where (idnt, rest) = break (== ' ' ) xs 
+tokenize ('/':'/':xs) = Comment cmnt : tokenize rest where (cmnt, rest) = break (== '\n') xs 
 tokenize ('\'':'h':xs) = BaseHex  : tokenize xs
 tokenize ('\'':'d':xs) = BaseDec  : tokenize xs
 tokenize ('\'':'o':xs) = BaseOct  : tokenize xs
 tokenize ('\'':'b':xs) = BaseBin  : tokenize xs
-tokenize (x:xs) | isAlpha x = if member anum keyw_map 
-                                then (keyw_map ! anum)                     : tokenize (dropWhile isLegalIdent xs)
-                                else Ident anum                            : tokenize (dropWhile isLegalIdent xs)
-                | isDigit x = NumLiteral (read (takeWhile isDigit (x:xs))) : tokenize (dropWhile isDigit xs)
-                | otherwise = error $ "Unexpected token: " ++ take 10 (x:xs)
-                where anum = takeWhile isLegalIdent (x:xs)
+tokenize (x:xs) | isDigit x = NumLiteral (read digits) : tokenize rest where (digits, rest) = span isDigit (x:xs) 
+tokenize (x:xs) | isAlpha x = case span isLegalIdent (x:xs) of
+                                 ("module"    , rest)  -> K_module    : tokenize rest
+                                 ("endmodule" , rest)  -> K_endmodule : tokenize rest
+                                 ("begin"     , rest)  -> K_begin     : tokenize rest
+                                 ("end"       , rest)  -> K_end       : tokenize rest
+                                 ("assign"    , rest)  -> K_assign    : tokenize rest
+                                 ("wire"      , rest)  -> K_wire      : tokenize rest
+                                 ("reg"       , rest)  -> K_reg       : tokenize rest
+                                 ("input"     , rest)  -> K_input     : tokenize rest
+                                 ("output"    , rest)  -> K_output    : tokenize rest
+                                 ("inout"     , rest)  -> K_inout     : tokenize rest
+                                 ( other      , rest)  -> Ident other : tokenize rest
+tokenize err = error $ "Unexpected token: " ++ take 10 err
 
 isLegalIdent :: Char -> Bool
 isLegalIdent c = isAlphaNum c || c == '_' || c == '$'
@@ -81,51 +80,25 @@ tokenize_file :: String -> IO [Token]
 tokenize_file s = readFile s >>= (return . no_comments . tokenize)
 
 
-expect :: Token -> State [Token] ()
-expect t = get >>= (\(x:xs) -> if x == t then put xs else error $ "Expected token <" ++ show t ++ ">, but got: " ++ show x)
 
-ident_value :: State [Token] String
-ident_value = do (i:xs) <- get
-                 case i of
-                  (Ident s) -> put xs >> return s
-                  otherwise -> error $ "Expected an identifier, but got: " ++ show i
+-----------------------------------------------------------------------------------------------------------------------
 
-consume :: Int -> State [Token] ()
-consume i = get >>= (put . (drop i))
+cur :: State [Token] Token
+cur = get >>= (\c -> case c of
+                       (x:_)     -> return x
+                       otherwise -> error "Unexpected end of input (cur)" )
 
-parse_port :: State [Token] Port
-parse_port = do st <- get
-                case st of
-                  (K_input :Ident s:xs) -> consume 2 >> return (Port s Input  Single)
-                  (K_inout :Ident s:xs) -> consume 2 >> return (Port s Inout  Single)
-                  (K_output:Ident s:xs) -> consume 2 >> return (Port s Output Single)
-                  (K_input :LBracket:NumLiteral hi:Colon:NumLiteral lo:RBracket:Ident s:xs) -> consume 7 >> return (Port s Input  (Bus hi lo))
-                  (K_inout :LBracket:NumLiteral hi:Colon:NumLiteral lo:RBracket:Ident s:xs) -> consume 7 >> return (Port s Inout  (Bus hi lo))
-                  (K_output:LBracket:NumLiteral hi:Colon:NumLiteral lo:RBracket:Ident s:xs) -> consume 7 >> return (Port s Output (Bus hi lo))
-                  otherwise             -> error $ "Expected a port spec but got: " ++ (show $ take 7 st)
+adv :: State [Token] ()
+adv = get >>= (\c -> case c of
+                       (_:xs)    -> put xs
+                       otherwise -> error "Unexpected end of input (adv)" )
 
-parse_ports :: State [Token] [Port]
-parse_ports = do expect LParen
-                 ports <- parse_ports_loop []
-                 expect RParen
-                 expect Semi
-                 return ports
+expt :: Token -> State [Token] ()
+expt t = get >>= (\(x:xs) -> if x == t then put xs 
+                                       else error $ "Expected token <" ++ show t ++ ">, but got: " ++ show x)
+
+-----------------------------------------------------------------------------------------------------------------------
 
 
-parse_ports_loop :: [Port] -> State [Token] [Port]
-parse_ports_loop pp = do p <- parse_port
-                         s <- get
-                         let current_plist = pp ++ [p]
-                         if head s /= Comma then return current_plist
-                                            else consume 1 >> parse_ports_loop current_plist
 
-parse_modul :: State [Token] Module
-parse_modul =  do s <- get
-                  expect K_module
-                  nm <- ident_value
-                  pp <- parse_ports
-                  return $ Module nm pp
-
-main = (fmap head getArgs) >>= tokenize_file >>= return . (runState parse_modul) >>= putStrLn . show
-
-
+main = undefined
